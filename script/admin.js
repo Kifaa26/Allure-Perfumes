7406bd9311f6b9850f56676d484651bcdad3632c
Kifaa26 <akifahpalekar@gmail.com>
2024-06-13 13:54:28 +0200

    updates

---
+++ script/admin.js
@@ -0,0 +1,132 @@
+let products = JSON.parse(localStorage.getItem('products')) || [];
+
+let sortedProducts = document.getElementById('adminSortProduct');
+let tableContent = document.querySelector('#table-products');
+
+
+function adminContent(args) {
+    try {
+        tableContent.innerHTML = "";
+        args?.forEach((product, i) => {
+            tableContent.innerHTML += `
+            <tr>
+                <td>${product.id}</td>
+                <td>${product.name}</td>
+                <td>${product.category}</td>
+                <td>${product.description}</td>
+                <td>R ${product.price}</td>
+                <td><img src="${product.img_url}" alt="${product.id}" class="img-thumbnail h-50 w-50"></td>
+                <td>
+                <div class="d-flex justify-content-around">
+                    <button class="btn btn-outline-success me-2" data-bs-toggle="modal" data-bs-target="#updateProduct${product.id}">Update</button>
+                    <button class="btn btn-outline-success ms-2" onclick="deleteProduct(${i})">Delete</button>
+                    <div class="modal fade" id="updateProduct${product.id}" tabindex="-1" aria-labelledby="updateProduct${product.id}" aria-hidden="true">
+                        <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                          <h1 class="modal-title fs-5" id="updateProduct${product.id}">Update Product</h1>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          <form>
+                          <div class="container">
+                          <input class="form-control m-2" type="text" placeholder="Enter item id" value="${product.id}" name="admin-id" id="admin-id${product.id}" required>
+                          <input class="form-control m-2" type="text" placeholder="Enter item name" value="${product.name}" name="admin-name" id="admin-name${product.id}" required>
+                          <input class="form-control m-2" type="text" placeholder="Enter item category" value="${product.category}" name="admin-category" id="admin-category${product.id}" required>
+                          <textarea class="form-control m-2" placeholder="Enter item description" required name="admin-description" id="admin-description${product.id}">${product.description}</textarea>
+                          <input class="form-control m-2" type="number" placeholder="Enter item price" value="${product.price}" name="admin-price" id="admin-price${product.id}" required>
+                          <input class="form-control m-2" type="url" placeholder="Enter image URL" value="${product.img_url}" name="admin-img_url" id="admin-img_url${product.id}" required>
+                          </div>
+                          </form>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-outline-success" data-bs-dismiss="modal">Close</button>
+                          <button type="button" class="btn btn-outline-success" onclick='new UpdateProduct(${JSON.stringify(product)}, ${i})'>Save changes</button>
+                        </div>
+                      </div>
+                        </div>
+                    </div>
+                </div>
+                </td>
+            </tr>
+            `;
+
+            
+        });
+    } catch (e) {
+        tableContent.innerHTML = `
+        <div class="d-flex justify-content-center">
+            <div class="spinner-border" role="status"></div>
+            <p>No Products Found</p>
+        </div>
+        `;
+    }
+}
+
+
+adminContent(products);
+
+function UpdateProduct(item, index) {
+    try {
+        const updatedProduct = {
+            id: item.id,
+            name: document.querySelector(`#admin-name${item.id}`).value,
+            category: document.querySelector(`#admin-category${item.id}`).value,
+            description: document.querySelector(`#admin-description${item.id}`).value,
+            price: parseInt(document.querySelector(`#admin-price${item.id}`).value, 10),
+            img_url: document.querySelector(`#admin-img_url${item.id}`).value
+        };
+
+        products[index] = updatedProduct;
+        localStorage.setItem('products', JSON.stringify(products));
+        adminContent(products);
+    } catch (e) {
+        alert('Unable to Edit the Products');
+    }
+}
+
+function deleteProduct(index) {
+    try {
+        products.splice(index, 1);
+        localStorage.setItem('products', JSON.stringify(products));
+        adminContent(products);
+    } catch (e) {
+        alert('Unable to Delete');
+    }
+}
+
+let highest = false;
+sortedProducts.addEventListener('click', () => {
+    try {
+        if (!highest) {
+            products.sort((a, b) => b.id - a.id);
+            highest = true;
+        } else {
+            products.sort((a, b) => a.id - b.id);
+            highest = false;
+        }
+        adminContent(products);
+    } catch (e) {
+        alert('This Function is under maintenance');
+    }
+});
+
+let adminSavedProduct = document.getElementById('saveProduct');
+adminSavedProduct.addEventListener('click', () => {
+    try {
+        const newProduct = {
+            id: products.length ? products[products.length - 1].id + 1 : 1, // auto increment ID
+            name: document.querySelector('#addName').value,
+            category: document.querySelector('#addCategory').value,
+            description: document.querySelector('#addDescription').value,
+            price: parseInt(document.querySelector('#addPrice').value, 10),
+            img_url: document.querySelector('#addImage').value
+        };
+
+        products.push(newProduct);
+        localStorage.setItem('products', JSON.stringify(products));
+        adminContent(products);
+    } catch (e) {
+        alert('Unable to Add new product');
+    }
+});
--- script/code.js
+++ script/code.js
@@ -5,7 +5,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 id: 1,
                 name: 'CHANEL COCO MADEMOISELLE',
                 category: 'Womens',
-                description: 'Top notes: Tonka bean, White musk, and Bourbon vanilla.',
+                description: 'Scents: Tonka bean, White musk, and Bourbon vanilla',
                 price: 2580.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/8d4e1e964b3b08b9a0c2bdfd8023ae4b.jpg?raw=true"
             },
@@ -13,7 +13,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 id: 2,
                 name: 'VERSACE CRYSTAL NOIR',
                 category: 'Womens',
-                description: 'Top notes: A combo of cardamom, pepper and ginger. Dry notes: Gardenia, Peony and Orange blossom.',
+                description: 'Scents: Cardamom, Pepper and Ginger, Gardenia, Peony and Orange blossom',
                 price: 2520.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/c5871c569793c01632f11330dade717d.jpg?raw=true"   
             },
@@ -21,7 +21,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 id: 3,
                 name: 'GUCCI OUD INTENSE',
                 category: 'Womens',
-                description: 'Top notes: Pear, Raspberry and saffron. Heart note: Bulgarian Rose and Orange Flower infusefloral elegance and warmth. Base notes" Natural Oud Oil and Patchouli provides depth and longevity to the scent.',
+                description: 'Scents: Pear, Raspberry, Saffron, Bulgarian rose, Orange Flower, Natural Oud Oil, Patchouli',
                 price: 2950.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/3e97ca77842dfee502727ebffe968f0f.jpg?raw=true"
             },
@@ -29,7 +29,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 id: 4,
                 name: 'YVES SAINT LAURENT BLACK OPIUM',
                 category: 'Womens',
-                description: 'The signature black coffee accord is paired with sensual vanilla, enriched by the softness of white flowers and orange blossom, set against a base of patchouli and omforting white musk.',
+                description: 'Scents: Vanilla, White flowers and Orange blossom, Patchouli, White musk',
                 price: 3200.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/10646a2556e212b7958081b7bbafff8b.jpg?raw=true"
             },
@@ -37,7 +37,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 id: 5,
                 name: 'LANCOME LA VIE EST BELLE',
                 category: 'Womens',
-                description: 'A fragrance with a powerful trail, with a perfect balance between the nobility of Iris, the depth of Patchouli, and an alluring vanilla.',
+                description: 'Scents: Iris, Patchouli, Vanilla',
                 price: 3300.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/9623c8983fd75ddcc9b2cd2f76ac2000.jpg?raw=true"
             },
@@ -45,7 +45,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
             id: 6,
                 name: 'DOLCE & GABANNA THE ONLY ONE',
                 category: 'Womens',
-                description: 'The head notes open with refined hints of bergamot, melding with pear and freesia to create a vivacious, delectable and unexpected bouqet. Heart bursts with violet accords, blending with rich coffe notes and charming Moroccan iris.',
+                description: 'Scents: Bergamot, Pear, Freesia, Rich coffee, Moroccan iris',
                 price: 3570.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/ed58fb5e2eb29c77a747f74fff3bb37c.jpg?raw=true"
             },
@@ -53,7 +53,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 id: 7,
                 name: 'ARMANI ACQUA DI GIO HOMME PROFUMO',
                 category: 'Mens',
-                description: 'Top notes: Peony, Musk, Pneapple, Lemon, Peach, Banana leaf and Violet. The dry down has notes of Musk, Ceder, Sandalwood, Styrax and Amber. ',
+                description: 'Scents: Peony, Musk, Pneapple, Lemon, Peach, Banana leaf, Violet, Musk, Ceder, Sandalwood, Amber ',
                 price: 4992.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/1d4b400ca58b4b21b167531bc57ca4d8.jpg?raw=true"
             },
@@ -61,7 +61,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 id: 8,
                 name: 'BLEU DE CHANEL',
                 category: 'Mens',
-                description: 'Top notes: Fresh citrus accord followed by ambery cedar. Woody nots are amplified by tonka bean and vanilla for heightend sensuality.',
+                description: 'Scents: Fresh citrus, Mmbery cedar, Tonka bean, Vanilla',
                 price: 2985.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/34cbc134e196d9168c041d25455cd302.jpg?raw=true"
             },
@@ -69,7 +69,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 id: 9,
                 name: 'BULGARI MAN IN BLACK',
                 category: 'Mens',
-                description: 'Intense composition of amber, leather, spices and woods.',
+                description: 'Scents: Amber, Leather, Spices, Woods',
                 price: 3050.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/7a09f098a9d92a48571f14c44b51a631.jpg?raw=true"
             },
@@ -77,7 +77,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 id: 10,
                 name: 'SAUVAGE DIOR',
                 category: 'Mens',
-                description: 'Top notes: Bergamot and lemon. Middle notes: Lavender and pepper. Base notes: Amber and patchouli.',
+                description: 'Scents: Bergamot, Lemon, Lavender, Pepper, Amber, Patchouli',
                 price: 2515.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/5aea3a6cc98b09c74975b3fa87e289cc.jpg?raw=true"
             },
@@ -85,7 +85,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 id: 11,
                 name: 'TOM FORD NOIR DE NOIR',
                 category: 'Mens',
-                description: 'Rich, feminine florals collide with the masculine earthiness of black truffle, vanilla and patchouli.',
+                description: 'Scents: Black truffle, Vanilla, Patchouli',
                 price: 4800.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/990e9369772c9a47f6b2cc260475092b.jpg?raw=true"
             },
@@ -93,7 +93,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 id: 12,
                 name: 'L HOMME YVES SAINT LAURENT',
                 category: 'Mens',
-                description: 'The opening is fresh: fresh lemon zest, fresh ginger burst and a nice vetiver in the background.',
+                description: 'Scents: Lemon zest, Ginger, Vetiver',
                 price: 2200.00,
                 img_url: "https://github.com/Kifaa26/images-js/blob/main/Listing/f690e5cbd7cd0855209af58de7a624bc.jpg?raw=true"
             }
